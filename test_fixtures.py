37e387cc968563444b953600c3d57be149eb72a4
Firex18ru <Firex1@yandex.ru>
2024-05-06 22:22:05 +0400

    Обновляю ДЗ

--- test_fixtures.py
+++ test_fixtures.py
@@ -1,13 +1,13 @@
 import time
-import pytest
 from selene import browser, by, have
-
 
 def test_search():
     browser.open('https://google.com')
-    time.sleep(3)
+#    time.sleep(3)
     browser.element(by.name('q')).type('asus graphics card').press_enter()
-    time.sleep(3)
-    browser.all('.web-result').first.element('.result__link').click()
+#   time.sleep(3)
+    browser.element('[id="search"]').should(have.text('asus graphics card'))
 
-    browser.should(have.title_containing('asus graphics card'))
+#    browser.all('.web-result').first.element('.result__link').click()
+
+#    browser.should(have.title_containing('asus/asus graphics card'))
