4775313f14d0e295a93af233a1105d09648d4e71
Firex18ru <Firex1@yandex.ru>
2024-05-07 18:20:58 +0400

    Исправил ДЗ

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
 import pytest
 from selene import browser
-import time
-@pytest.fixture(scope="session",autouse=True)
+
+
+@pytest.fixture(scope="session", autouse=True)
 def browser_settings():
     browser.config.window_height = 768
     browser.config.window_width = 1024
-#    time.sleep(3)
+
     yield
     browser.quit()
--- test_fixtures.py
+++ test_fixtures.py
@@ -1,13 +1,17 @@
-import time
 from selene import browser, by, have
+
 
 def test_search():
     browser.open('https://google.com')
-    time.sleep(3)
+
     browser.element(by.name('q')).type('asus graphics card').press_enter()
-    time.sleep(3)
-    browser.element('[id="search"]').should(have.text('asus graphics card'))
 
-#    browser.all('.web-result').first.element('.result__link').click()
+    browser.element('[id="search"]').should(have.text('graphics card'))
 
-#    browser.should(have.title_containing('asus/asus graphics card'))
+
+def test_wrong_search():
+    browser.open('https://google.com')
+
+    browser.element(by.name('q')).type('неверный hfndkcvkl запрос').press_enter()
+
+    browser.element('[id="search"]').should(have.text('pytest'))
